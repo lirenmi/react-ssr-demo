8c77a8228cf1cfcd89745b39e0a00f9ad2e61827
lirenmi <lirenmi@pop-os.localdomain>
2021-06-17 21:11:40 +0800

    2. use koa in typescript

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,11 @@
-const Koa = require("koa");
+import Koa from 'koa';
 
 const app = new Koa();
 const port = 3000;
 
 function main() {
   app.use((ctx) => {
-    ctx.body = "hello world";
+    ctx.body = 'hello world';
   });
   app.listen(port);
   console.info(`App listening on port: ${port}`);
